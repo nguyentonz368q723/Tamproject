533750085019b9aa98b60c1810be080c83a264aa
Nguyen Huu Tam Ton <huutam93ndu1516@gmail.com>
2024-04-09 23:16:37 -0500

    still cannot login to dashboard

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -25,32 +25,58 @@ exports.register = async (req, res) => {
   }
 }
 
+// exports.login = async (req, res) => {
+//   const { name, password } = req.body;
+
+//   try {
+//       const user = await User.findOne({ name });
+//       if (!user) {
+//           return res.status(404).json({ message: "User not found" });
+//       }
+//       const isMatch = await bcrypt.compare(password, user.password);
+//       if (!isMatch) {
+//           return res.status(400).json({ message: "Invalid credentials" });
+//       }
+//       const token = jwt.sign({ userId: user._id }, 'your_secret_key', { expiresIn: '1y' });
+//       res.status(200).json({ 
+//         name: user.name, 
+//         token, 
+//         password: user.hashedPassword, 
+//         email: user.email, 
+//         id: user._id,
+//         phonenumber: user.phonenumber,
+//         redirectUrl: './dashboard'
+//       });
+//       res.render('../views/dashboard.ejs');
+
+//   } catch (error) {
+//       console.error('Login error:', error);
+//       res.status(500).json({ message: "Internal server error" });
+//   }
+// };
+
 exports.login = async (req, res) => {
   const { name, password } = req.body;
 
   try {
-      const user = await User.findOne({ name });
-      if (!user) {
-          return res.status(404).json({ message: "User not found" });
-      }
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-          return res.status(400).json({ message: "Invalid credentials" });
-      }
-      const token = jwt.sign({ userId: user._id }, 'your_secret_key', { expiresIn: '1y' });
-      res.status(200).json({ 
-        name: user.name, 
-        token, 
-        password: user.hashedPassword, 
-        email: user.email, 
-        id: user._id,
-        phonenumber: user.phonenumber,
-        redirectUrl: './dashboard'
-      });
-      res.render('../views/dashboard.ejs');
+    const user = await User.findOne({ name });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, 'your_secret_key', { expiresIn: '1y' });
+
+    res.status(200).json({ redirectUrl: '../views/dashboard' });
+    // Redirect to dashboard view
+    // res.render('../views/dashboard');
 
   } catch (error) {
-      console.error('Login error:', error);
-      res.status(500).json({ message: "Internal server error" });
+    console.error('Login error:', error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
